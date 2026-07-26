805648c3a385c4abf601ca0f8633fe4911053506
charmpic <moecharm.dev@gmail.com>
2026-07-27 02:03:28 +0900

    test(mir): cover raw body statement input parity

--- src/mir/builder/raw_expression_dispatch/tests.rs
+++ src/mir/builder/raw_expression_dispatch/tests.rs
@@ -1,8 +1,14 @@
 use crate::ast::{ASTNode, BinaryOperator, LiteralValue, Span};
 use crate::mir::{MirBuilder, MirInstruction};
 
-use super::input_view::RawLegacyExpressionInputV1;
+use super::input_view::{
+    RawLegacyBodyInputV1, RawLegacyExpressionInputV1, RawLegacyStatementInputV1,
+};
 use crate::mir::builder::recursive_child_lowering::RawLegacyChildLoweringPortV1;
+use crate::mir::builder::stmts::block_stmt::{
+    build_block, build_block_input_view_with_port_v1, build_statement,
+    build_statement_input_view_with_port_v1,
+};
 
 fn integer(value: i64) -> ASTNode {
     ASTNode::Literal {
@@ -52,4 +58,47 @@ fn legacy_input_view_and_legacy_facade_share_one_matcher_behavior() {
 
     assert_eq!(view_value, facade_value);
     assert_eq!(instructions(&view), instructions(&facade));
+}
+
+#[test]
+fn legacy_body_and_statement_facades_preserve_input_view_parity() {
+    let body = vec![integer(1), add(integer(2), integer(3))];
+
+    let mut body_facade = MirBuilder::new();
+    body_facade.enter_function_for_test("raw_input_body/0".to_string());
+    let facade_body_value = build_block(&mut body_facade, body.clone()).unwrap();
+
+    let mut body_view = MirBuilder::new();
+    body_view.enter_function_for_test("raw_input_body/0".to_string());
+    let mut body_port = RawLegacyChildLoweringPortV1;
+    let view_body_value = build_block_input_view_with_port_v1(
+        &mut body_view,
+        &mut body_port,
+        RawLegacyBodyInputV1::new(body),
+    )
+    .unwrap();
+
+    assert_eq!(view_body_value, facade_body_value);
+    assert_eq!(instructions(&body_view), instructions(&body_facade));
+
+    let statement = add(integer(4), integer(5));
+    let mut statement_facade = MirBuilder::new();
+    statement_facade.enter_function_for_test("raw_input_statement/0".to_string());
+    let facade_statement_value = build_statement(&mut statement_facade, statement.clone()).unwrap();
+
+    let mut statement_view = MirBuilder::new();
+    statement_view.enter_function_for_test("raw_input_statement/0".to_string());
+    let mut statement_port = RawLegacyChildLoweringPortV1;
+    let view_statement_value = build_statement_input_view_with_port_v1(
+        &mut statement_view,
+        &mut statement_port,
+        RawLegacyStatementInputV1::new(statement),
+    )
+    .unwrap();
+
+    assert_eq!(view_statement_value, facade_statement_value);
+    assert_eq!(
+        instructions(&statement_view),
+        instructions(&statement_facade)
+    );
 }
